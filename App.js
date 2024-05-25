fbc9417faf6dbba36756898ed1238cd2fc37e47b
Momina Amjad <97848225+mominaamjad@users.noreply.github.com>
2024-05-25 15:29:22 +0500

    Update App.js

--- App.js
+++ App.js
@@ -17,20 +17,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-<<<<<<< backend
-=======
-
-import AdminMainScreen from './components/admin/AdminMainScreen';
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
-};
->>>>>>> main
 
 function Section({children, title}) {
   const isDarkMode = useColorScheme() === 'dark';
