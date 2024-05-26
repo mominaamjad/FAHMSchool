bf63e41048c8266d8c4ebd9dc6dc6e52ec3005ec
amnasohaib <amnasohaib2003@gmail.com>
2024-05-26 22:55:46 +0500

    timetable dropdown

--- components/admin/FeeScreen.js
+++ components/admin/FeeScreen.js
@@ -19,11 +19,11 @@ import Card from "../layouts/Card";
 
 const FeeScreen = () => {
 
-        // for pop-up 
+  // for pop-up 
   const [modalVisible, setModalVisible] = useState(false);
 
   // for dropdown 
-  const [value, setValue] = useState();
+  const [value, setValue] = useState('allClasses');
   const [open, setOpen] = useState(false);
   const [items, setItems] = useState([
     { label: 'All Classes', value: 'allClasses' },
@@ -33,26 +33,26 @@ const FeeScreen = () => {
     { label: 'Class 4', value: 'class4' },
     { label: 'Class 5', value: 'class5' },
     { label: 'Class 6', value: 'class6' },
-]);
+  ]);
 
   // students list to be displayed
   const [students, setStudents] = useState([
     {
-        class: 'class1' , regNo: 'fa21-bcs-011', name: 'amna sohaib', fathername: 'sohaib ahmed',
-        dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
-        residence: 'smth', dateOfAdmission: '13/7/2021'
+      class: 'class1', regNo: 'fa21-bcs-011', name: 'amna sohaib', fathername: 'sohaib ahmed',
+      dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
+      residence: 'smth', dateOfAdmission: '13/7/2021'
     },
     {
-        class: 'class2' , regNo: 'fa21-bcs-011', name: 'haneen ehsan', fathername: 'sohaib ahmed',
-        dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
-        residence: 'smth', dateOfAdmission: '13/7/2021'
+      class: 'class2', regNo: 'fa21-bcs-011', name: 'haneen ehsan', fathername: 'sohaib ahmed',
+      dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
+      residence: 'smth', dateOfAdmission: '13/7/2021'
     },
     {
-        class: 'class3' , regNo: 'fa21-bcs-0024', name: 'fasiha arshad', fathername: 'sohaib ahmed',
-        dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
-        residence: 'smth', dateOfAdmission: '13/7/2021'
+      class: 'class3', regNo: 'fa21-bcs-0024', name: 'fasiha arshad', fathername: 'sohaib ahmed',
+      dob: '29/11/2003', gender: 'female', caste: 'smth', occupation: 'smth',
+      residence: 'smth', dateOfAdmission: '13/7/2021'
     },
-]);
+  ]);
 
 
   // to set index of class array for assigning true or false
@@ -93,10 +93,10 @@ const FeeScreen = () => {
 
   handleFilteredList = () => {
     if (value == 'allClasses') {
-        setList(students)
+      setList(students)
     }
     else {
-    setList(() => students.filter((element) => element.class.toLowerCase().includes(value)))
+      setList(() => students.filter((element) => element.class.toLowerCase().includes(value)))
     }
   }
 
@@ -104,202 +104,202 @@ const FeeScreen = () => {
     <View>
 
 
-    {/* change the function to find fee instead of students  */}
-    
-    <View style={styles.searchBar}>
-        <TextInput style={styles.search} 
-        label="Search" 
-        onChangeText={(text) => { searchItem(text) }}
-        value={search}
-        onBlur={() => { setSearch(""); setList(students); }}
-            />
-         <Icon name="magnify" size={30} style={styles.searchIcon}/>
+      {/* change the function to find fee instead of students  */}
 
-         <DropDownPicker
-              textStyle = {styles.dropdownText}
+      <View style={styles.searchBar}>
+        <TextInput style={styles.search}
+          label="Search"
+          onChangeText={(text) => { searchItem(text) }}
+          value={search}
+          onBlur={() => { setSearch(""); setList(students); }}
+        />
+        <Icon name="magnify" size={30} style={styles.searchIcon} />
+
+        <DropDownPicker
+          textStyle={styles.dropdownText}
+          style={styles.dropdown}
+          dropDownContainerStyle={styles.dropdown}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          onChangeValue={() => handleFilteredList()}
+        />
+
+      </View>
+
+
+      <ScrollView style={{ zIndex: -1 }}>
+        {list.map((element, index) =>
+          <TouchableOpacity onPress={() => { setModalVisible(true); setIndex(index) }}>
+            <Card name={element.name} regNo={element.regNo} paid={element.paid} cardType="fee"></Card>
+          </TouchableOpacity>
+        )}
+      </ScrollView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalHeading}>Class Name</Text>
+            <Text style={styles.modalText}>Choose Teacher</Text>
+
+            <DropDownPicker
+              textStyle={styles.dropdownText}
               style={styles.dropdown}
-              dropDownContainerStyle={styles.dropdown}
               open={open}
               value={value}
               items={items}
               setOpen={setOpen}
               setValue={setValue}
               setItems={setItems}
-              onChangeValue={()=>handleFilteredList()}
-          />
+            />
 
-      </View>
+            <View style={styles.btnRow}>
 
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.cancelText}>Cancel</Text>
+              </TouchableOpacity>
 
-      <ScrollView style={{zIndex: -1}}>
-        {list.map((element, index) =>
-          <TouchableOpacity onPress={() => { setModalVisible(true); setIndex(index) }}>
-            <Card name={element.name} regNo={element.regNo} paid ={element.paid} cardType="fee"></Card>
-          </TouchableOpacity>
-        )}
-      </ScrollView>
+              <TouchableOpacity
+                style={styles.buttonSubmit}
+                onPress={() => handlePaidClass()}>
+                <Text style={styles.submitText}>Submit</Text>
+              </TouchableOpacity>
 
-        <Modal
-            animationType= "slide"   
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-            setModalVisible(!modalVisible);
-            }}>
-            <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <Text style={styles.modalHeading}>Class Name</Text>
-                    <Text style={styles.modalText}>Choose Teacher</Text>
-        
-                    <DropDownPicker
-                        textStyle = {styles.dropdownText}
-                        style={styles.dropdown}
-                        open={open}
-                        value={value}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                    />
-
-                    <View style = {styles.btnRow}>
-
-                      <TouchableOpacity
-                          style={styles.cancelButton}
-                          onPress={() => setModalVisible(!modalVisible)}>
-                          <Text style={styles.cancelText}>Cancel</Text>
-                      </TouchableOpacity>
-
-                      <TouchableOpacity
-                          style={styles.buttonSubmit}
-                          onPress={() => handlePaidClass()}>
-                          <Text style={styles.submitText}>Submit</Text>
-                      </TouchableOpacity>
-                    
-                    </View>
-                </View>
             </View>
-        </Modal>
-        
+          </View>
         </View>
-        
-    );
+      </Modal>
+
+    </View>
+
+  );
 }
 
 styles = StyleSheet.create({
 
-    searchBar: {
-        flexDirection: 'row',
-        marginTop: 20,
-        margin: 10,
-        backgroundColor: 'lavender',
-        width: 250,
-        padding: 3,
-        
-        borderRadius: 30
-        
-      },
-      search: {
-        height: 40,
-        width: 200,
-        color: "#000000",
-        paddingLeft: 10,
-        fontSize: 14,
-        fontFamily: 'Poppins-Regular'
-        
-      },
-    
-      searchIcon:{
-        alignSelf: "center",
-        backgroundColor: "#8349EA",
-        padding: 5,
-        borderRadius: 20
-        
-      },
+  searchBar: {
+    flexDirection: 'row',
+    marginTop: 20,
+    margin: 10,
+    backgroundColor: 'lavender',
+    width: 250,
+    padding: 3,
 
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
-      },
+    borderRadius: 30
 
+  },
+  search: {
+    height: 40,
+    width: 200,
+    color: "#000000",
+    paddingLeft: 10,
+    fontSize: 14,
+    fontFamily: 'Poppins-Regular'
 
-      modalView: {
-        margin: 20,
-        padding: 35,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        // alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: '#8349EA',
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 4,
-        backgroundColor: '#FFFFFF'
-      },
+  },
 
-      btnRow:{
-        flexDirection:'row', 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        marginTop: 20
-      },
+  searchIcon: {
+    alignSelf: "center",
+    backgroundColor: "#8349EA",
+    padding: 5,
+    borderRadius: 20
 
-      buttonSubmit: {
-        borderRadius: 17,
-        paddingHorizontal: 22,
-        paddingVertical: 10,
-        elevation: 2,
-        backgroundColor: '#8349EA',
-        marginLeft: 10,
-        marginRight: 10
-      },
-    
-      cancelText:{ 
-        color: '#6D6D6D',
-        paddingLeft: 20,
-        paddingRight: 20,
-        fontFamily: 'Poppins-Light'
-      
-      },
+  },
 
-      submitText: {
-        color: 'white',
-        textAlign: 'center',
-        fontFamily: 'Poppins-SemiBold',
-    
-      },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
 
 
-      modalHeading: {
-        fontSize: 20,
-        fontFamily: 'Poppins-Bold',
-        marginBottom: 15,
-        textAlign: 'center',
-        color: 'black'
-      },
+  modalView: {
+    margin: 20,
+    padding: 35,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    // alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#8349EA',
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 4,
+    backgroundColor: '#FFFFFF'
+  },
+
+  btnRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+
+  buttonSubmit: {
+    borderRadius: 17,
+    paddingHorizontal: 22,
+    paddingVertical: 10,
+    elevation: 2,
+    backgroundColor: '#8349EA',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  cancelText: {
+    color: '#6D6D6D',
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontFamily: 'Poppins-Light'
+
+  },
+
+  submitText: {
+    color: 'white',
+    textAlign: 'center',
+    fontFamily: 'Poppins-SemiBold',
+
+  },
 
 
-      modalText: {
-        marginBottom: 15,
-        fontFamily: 'Poppins-Medium',
-        textAlign: 'left',
-        color: 'black'
-      },
+  modalHeading: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Bold',
+    marginBottom: 15,
+    textAlign: 'center',
+    color: 'black'
+  },
 
 
-      
-    dropdown: {
-      marginLeft: 20,
-      width: 120,
-      backgroundColor: '#F4F4F4',
-      borderColor: '#8349EA'
-    },
+  modalText: {
+    marginBottom: 15,
+    fontFamily: 'Poppins-Medium',
+    textAlign: 'left',
+    color: 'black'
+  },
 
-    dropdownText:{
-      fontFamily: 'Poppins-Medium'
-    }
+
+
+  dropdown: {
+    marginLeft: 20,
+    width: 120,
+    backgroundColor: '#F4F4F4',
+    borderColor: '#8349EA'
+  },
+
+  dropdownText: {
+    fontFamily: 'Poppins-Medium'
+  }
 })
 
 export default FeeScreen
--- components/admin/TimetableScreen.js
+++ components/admin/TimetableScreen.js
@@ -1,46 +1,87 @@
-import { ScrollView,
-    View, 
+import React, { useState } from 'react';
+import {
+    ScrollView,
+    View,
     Text,
     StyleSheet,
     Image,
     TouchableOpacity
 
- } from "react-native"
+} from "react-native"
 
+
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const TimetableScreen = () => {
 
+    // for dropdown 
+    const [value, setValue] = useState();
+    const [open, setOpen] = useState(false);
+    const [items, setItems] = useState([
+        { label: 'All Classes', value: 'allClasses' },
+        { label: 'Class 1', value: 'class1' },
+        { label: 'Class 2', value: 'class2' },
+        { label: 'Class 3', value: 'class3' },
+        { label: 'Class 4', value: 'class4' },
+        { label: 'Class 5', value: 'class5' },
+        { label: 'Class 6', value: 'class6' },
+    ]);
 
-    return(
-        <ScrollView>
+    // const [classes, setClass] = useState([
+    //     // example data for now
+    //         { class: 'Class1', table: '../assets/timetable.jpeg'},
+    //         { class: 'Class2', table: '../assets/timetable.jpeg'},
+    //         { class: 'Class3', table: '../assets/timetable.jpeg'},
+    //     ])
+
+    // handleFilteredList = () => {
+
+    //     setClass(() => classes.filter((element) => element.class.toLowerCase().includes(value)))
+
+    //   }
 
 
-            {/* put the dropdown before this  */}
+    return (
+        <View >
+            <DropDownPicker
+                textStyle={styles.dropdownText}
+                style={styles.dropdown}
+                dropDownContainerStyle={styles.dropdown}
+                open={open}
+                value={value}
+                items={items}
+                setOpen={setOpen}
+                setValue={setValue}
+                setItems={setItems}
+                onChangeValue={() => handleFilteredList()}
+            />
 
             <View>
-                <Image source = {require('../assets/timetable.jpeg') } style = {styles.pic}/>
-            </View>
+                <View>
+                    <Image source={require('../assets/timetable.jpeg')} style={styles.pic} />
+                </View>
 
-            <View>
-                <TouchableOpacity style={styles.buttonUpload}>
-                    <Text style={styles.uploadText}>Upload</Text>
-                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity style={styles.buttonUpload}>
+                        <Text style={styles.uploadText}>Upload</Text>
+                    </TouchableOpacity>
 
+                </View>
             </View>
-        </ScrollView>
-    
+        </View>
+
     );
 }
 
 const styles = StyleSheet.create({
 
-    main:{
+    main: {
         color: "#000000",
         fontSize: 24,
         fontFamily: "Poppins-Light"
     },
 
-    pic:{ 
+    pic: {
         width: 340,
         height: 220,
         alignSelf: "center",
@@ -52,7 +93,7 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: 'center',
         fontFamily: 'Poppins-SemiBold',
-    
+
     },
 
     buttonUpload: {
@@ -65,8 +106,19 @@ const styles = StyleSheet.create({
         marginRight: 10,
         marginVertical: 60
     },
+
+    dropdown: {
+        marginLeft: 20,
+        width: 120,
+        backgroundColor: '#F4F4F4',
+        borderColor: '#8349EA'
+    },
+
+    dropdownText: {
+        fontFamily: 'Poppins-Medium'
+    }
 }
-   
+
 )
 
 export default TimetableScreen
