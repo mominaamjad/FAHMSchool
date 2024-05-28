09c85d3ff5b635d01fb6fcf167b9d91701b962eb
haneenehsan07 <haneenehsan07@gmail.com>
2024-05-28 16:34:56 +0500

    Changes in Admin Dashboard

--- components/admin/AdminDashboard.js
+++ components/admin/AdminDashboard.js
@@ -1,20 +1,148 @@
+import { useState } from "react";
 import { ScrollView,
     View, 
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 
  } from "react-native"
+
+ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 const AdminDashboard = () => {
 
+    const [selectedTab, setSelected] = useState(0)
+
 
     return(
         <ScrollView>
-            <Text>Welcome, Admin</Text>
+
+            <View style = {styles.switchBox} >
+
+                
+                <TouchableOpacity style={selectedTab === 0 ? styles.switchOn : styles.switchOff} 
+                    onPress={() => {
+                    setSelected(0)
+                    }}>
+
+                    <Text style={selectedTab === 0 ? styles.currentText : styles.otherText} > 
+                        Report 1
+                    </Text>
+
+                </TouchableOpacity>
+
+                
+
+                <TouchableOpacity style={selectedTab === 1 ? styles.switchOn : styles.switchOff} 
+                    onPress={() =>{
+                    setSelected(1)
+                    }}>
+
+                    <Text style={selectedTab === 1 ? styles.currentText : styles.otherText}> 
+                        Report 2
+                    </Text>
+
+                </TouchableOpacity >
+
+            </View>
+
+            <View>
+                <TouchableOpacity
+                    style={styles.buttonDownload}
+                    onPress={() => { setAddModalVisible(true); }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Icon name="download" size={24} color='white' />
+                        <Text style={styles.downloadText}> Download Report</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView>
+                 {selectedTab == 0? <Text style = {{color: "black"}}> yeh pehli hai </Text> : <Text style = {{color: "black"}}>yeh dusri hai </Text> }
+            </ScrollView>
+
+            
+
+        
+
         </ScrollView>
         
     );
 }
+
+const styles = StyleSheet.create({
+
+    switchBox:{
+        width: 250,
+        height: 50,
+        display:'flex',
+        flexDirection:'row',
+
+        borderWidth: 0.3,
+        borderRadius: 15,
+        alignSelf: 'center',
+        marginTop: 20
+    },
+
+    switchOn:{
+        width: 125,
+        height: 50,
+        backgroundColor: '#8349EA',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+
+    },
+
+    switchOff:{
+        width: 125,
+        height: 50,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    currentText: {
+        alignSelf: 'center',
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 14,
+        color: '#FFFFFF'
+
+    },
+
+    otherText: {
+        alignSelf: 'center',
+        fontFamily: 'Poppins-Regular',
+        fontSize: 14,
+        color: '#000000'
+
+    },
+
+    buttonDownload: {
+        width: 150,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+        borderRadius: 17,
+        paddingHorizontal: 22,
+        paddingVertical: 10,
+        backgroundColor: '#8349EA',
+        marginTop: 20,
+        marginRight: 12,
+        marginBottom: 10,
+    },
+
+    downloadText: {
+        color: 'white',
+        fontSize: 12,
+        fontFamily: 'Poppins-Medium',
+        alignSelf: 'center',
+        marginTop: 5
+    },
+
+
+
+})
 
 export default AdminDashboard
