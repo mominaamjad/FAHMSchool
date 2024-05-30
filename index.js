d087536617f1e1ed3f92f99de1796b17301d7d8c
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-05-30 15:58:54 +0500

    fee

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
+
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import FeeScreen from './components/admin/FeeScreen';
+import RecordsScreen from './components/admin/RecordsScreen';
 
-AppRegistry.registerComponent(appName, () => FeeScreen);
-
+AppRegistry.registerComponent(appName, () => RecordsScreen);
